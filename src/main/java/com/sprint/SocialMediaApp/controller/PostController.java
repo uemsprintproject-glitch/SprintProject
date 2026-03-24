3aa4dcb158cc7877e3f00ef5d9f3556dc567f00b
DSagnik24 <sagnikduttaofficial18@gmail.com>
2026-03-24 16:00:40 +0530

    Integrated Exception Handling

--- src/main/java/com/sprint/SocialMediaApp/controller/PostController.java
+++ src/main/java/com/sprint/SocialMediaApp/controller/PostController.java
@@ -1,8 +1,35 @@
 package com.sprint.SocialMediaApp.controller;
+
+import com.sprint.SocialMediaApp.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-
+@Controller
+@RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
+    @GetMapping("/home")
+    public String getHomePage(Model model) {
+        model.addAttribute("posts", postService.getAllPosts());
+        return "home";
+    }
+
+    @PostMapping("/posts")
+    public String createPost(@RequestParam Integer userId,
+                             @RequestParam String content) {
+
+        postService.createPost(userId, content);
+
+        return "redirect:/post";
+    }
+
+    @GetMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable Integer id) {
+        postService.deletePost(id);
+        return "redirect:/home";
+    }
 }
