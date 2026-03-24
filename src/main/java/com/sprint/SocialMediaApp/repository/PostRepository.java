e7b4654258150907d98d342d2231ece2cecb129c
DSagnik24 <sagnikduttaofficial18@gmail.com>
2026-03-24 12:30:53 +0530

    updating merge

--- src/main/java/com/sprint/SocialMediaApp/repository/PostRepository.java
+++ src/main/java/com/sprint/SocialMediaApp/repository/PostRepository.java
@@ -1,19 +1,6 @@
 package com.sprint.SocialMediaApp.repository;
 
-<<<<<<< HEAD
-import com.sprint.SocialMediaApp.entity.Post;
-import com.sprint.SocialMediaApp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface PostRepository extends JpaRepository<Post,Integer> {
-
-    List<Post> findByUser(User user);
-
-    List<Post> findAllByOrderByTimestampDesc();
-}
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.sprint.SocialMediaApp.entity.Post;
@@ -25,4 +12,4 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
     List<Post> findByUserUserID(int userId);
 }
->>>>>>> origin/feature-ashmit
+
